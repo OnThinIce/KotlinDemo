cd1edb14faacd0590e93c84ea1d2917e26b1f3f3
OnThinIce <764958658@qq.com>
2019-12-16 16:39:13 +0800

    no message

---
+++ app/src/main/java/com/zl/my/kt/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.zl.my.kt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
